03f4e5f1cbd3974d5cb9534bf9536e243ac66e8b
Soudeepan Biswas <114162231+soudeepan@users.noreply.github.com>
2022-09-24 09:11:43 +0530

    Add files via upload

---
+++ Watch&Color/js/app.js
@@ -0,0 +1,125 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.to(".title", 
+{
+    scrollTrigger:
+    {
+        trigger:".title",
+        
+        scrub:true,
+
+        start:"top top",
+        end:"50% 30%",
+    },
+    backgroundColor:'#FFFFFF',
+    color:'black',
+});
+
+gsap.to(".b1", 
+{
+    scrollTrigger:
+    {
+        trigger:".b1",
+       
+        scrub:true,
+
+        start:"top bottom",
+        end:"bottom top",
+    },
+    y:"50vh",
+    x:"2vh",
+});
+
+gsap.to(".b2", 
+{
+    scrollTrigger:
+    {
+        trigger:".b2",
+        
+        scrub:true,
+
+        start:"top bottom",
+        end:"bottom top",
+    },
+    y:"10vh",
+    x:"-5vh",
+});
+
+gsap.to(".b3", 
+{
+    scrollTrigger:
+    {
+        trigger:".b3",
+       
+        scrub:true,
+
+        start:"top bottom",
+        end:"bottom top",
+    },
+    y:"40vh",
+    x:"2vh",
+});
+
+gsap.to(".b4", 
+{
+    scrollTrigger:
+    {
+        trigger:".b4",
+       
+        scrub:true,
+
+        start:"top bottom",
+        end:"bottom top",
+    },
+    y:"30vh",
+    x:"-7vh",
+});
+
+gsap.to(".b5", 
+{
+    scrollTrigger:
+    {
+        trigger:".b5",
+       
+        scrub:true,
+
+        start:"top bottom",
+        end:"bottom top",
+    },
+    y:"35vh",
+    x:"4vh",
+});
+
+gsap.to(".b6", 
+{
+    scrollTrigger:
+    {
+        trigger:".b6",
+       
+        scrub:true,
+
+        start:"top bottom",
+        end:"bottom top",
+    },
+    y:"34vh",
+    x:"-4vh",
+});
+
+gsap.to(".c1", {scrollTrigger:{trigger:".c1",scrub:true,start:"top bottom", end:"bottom top",},y:"-20vh",x:"-140vw",rotation:-300});
+gsap.to(".c2", {scrollTrigger:{trigger:".c2",scrub:true,start:"top bottom", end:"bottom top",},y:"-26vh",x:"140vw",rotation:300});
+gsap.to(".c3", {scrollTrigger:{trigger:".c3",scrub:true,start:"top bottom", end:"bottom top",},y:"-29vh",x:"-140vw",rotation:-300});
+gsap.to(".c4", {scrollTrigger:{trigger:".c4",scrub:true,start:"top bottom", end:"bottom top",},y:"-21vh",x:"140vw",rotation:300});
+gsap.to(".c5", {scrollTrigger:{trigger:".c5",scrub:true,start:"top bottom", end:"bottom top",},y:"-24vh",x:"-140vw",rotation:-300});
+gsap.to(".c6", {scrollTrigger:{trigger:".c6",scrub:true,start:"top bottom", end:"bottom top",},y:"-32vh",x:"140vw",rotation:300});
+gsap.to(".c7", {scrollTrigger:{trigger:".c7",scrub:true,start:"top bottom", end:"bottom top",},y:"-29vh",x:"-140vw",rotation:-300});
+gsap.to(".c8", {scrollTrigger:{trigger:".c8",scrub:true,start:"top bottom", end:"bottom top",},y:"-23vh",x:"140vw",rotation:300});
+gsap.to(".c9", {scrollTrigger:{trigger:".c9",scrub:true,start:"top bottom", end:"bottom top",},y:"-20vh",x:"-140vw",rotation:-300});
+gsap.to(".c10", {scrollTrigger:{trigger:".c10",scrub:true,start:"top bottom", end:"bottom top",},y:"-35vh",x:"140vw",rotation:300});
+gsap.to(".c11", {scrollTrigger:{trigger:".c11",scrub:true,start:"top bottom", end:"bottom top",},y:"-20vh",x:"-140vw",rotation:-300});
+gsap.to(".c12", {scrollTrigger:{trigger:".c12",scrub:true,start:"top bottom", end:"bottom top",},y:"-25vh",x:"140vw",rotation:300});
+gsap.to(".c13", {scrollTrigger:{trigger:".c13",scrub:true,start:"top bottom", end:"bottom top",},y:"-32vh",x:"-140vw",rotation:-300});
+gsap.to(".c14", {scrollTrigger:{trigger:".c14",scrub:true,start:"top bottom", end:"bottom top",},y:"-20vh",x:"140vw",rotation:300});
+gsap.to(".c15", {scrollTrigger:{trigger:".c15",scrub:true,start:"top bottom", end:"bottom top",},y:"-28vh",x:"-140vw",rotation:-300});
+gsap.to(".c16", {scrollTrigger:{trigger:".c16",scrub:true,start:"top bottom", end:"bottom top",},y:"-10vh",x:"135vw",rotation:300});
+gsap.to(".c17", {scrollTrigger:{trigger:".c17",scrub:true,start:"top bottom", end:"bottom top",},y:"-17vh",x:"-130vw",rotation:-300});
